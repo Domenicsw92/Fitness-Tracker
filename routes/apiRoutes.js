fc2cee46d635dc9680ba096a5e1a6d3572a329c9
Domenic Wilhelm <Domenicsw92@gmail.com>
2021-06-25 10:52:18 -0600

    saving issues

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -24,7 +24,8 @@ router.post("/api/workouts", (req, res) => {
 });
 
 router.put("/api/workouts/:id", ({ body, params }, res) => {
-    db.Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+    db.Workout.findByIdAndUpdate(params.id, { $push: { exercises: body }}, {new: true,
+        runValidators: true})
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
