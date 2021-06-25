ab66d03e89524dfb2baa1e872209688b76a50d46
Domenic Wilhelm <Domenicsw92@gmail.com>
2021-06-25 11:03:18 -0600

    server changes

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var mongoose = require("mongoose");
+var logger = require("morgan")
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-
+app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(require("./routes/apiRoutes"))
-app.use(require("./routes/viewRoutes"))
+
 
 // require("./routes/apiRoutes")(app);
 // require("./routes/viewRoutes")(app);
@@ -19,6 +19,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useCreateIndex: true,
   useFindAndModify: false
 });
+
+app.use(require("./routes/apiRoutes"))
+app.use(require("./routes/viewRoutes"))
 
 app.listen(PORT, function() {
   console.log(`Now listening on port: ${PORT}`);
